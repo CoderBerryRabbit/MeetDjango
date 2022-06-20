4acd54bcb8bcea95f8e9099e92d0e7fa4cabe41d
CoderBerryRabbit <CoderChaos@yeah.net>
2022-06-21 07:20:03 +0800

    feat(polls): :sparkles: Create app: polls

---
+++ mysite/polls/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author   : CoderBerryRabbit
+# @Email    : CoderChaos@yeah.net
+# @Time     : 2022/6/21 7:16
+# @File     : urls.py python3.9
+# @Software : PyCharm MeetDjango
+# @Desc     : TODO
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
